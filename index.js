4b6b2dfb6d4a84859cb591d8716189ecd6d15022
Jaytun Kankotiya <jaytunkankotiya81@gmail.com>
2025-10-16 17:47:58 -0400

    updated version

--- index.js
+++ index.js
@@ -80,10 +80,8 @@ app.delete("/students/:id", async (req, res) => {
   }
 });
 
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
-const serverless = require('serverless-http');
-module.exports.handler = serverless(app);
